2ee701bf5e17dcb8f1ee1da0d2311c9c35f46c56
Suspicious <alex@kaucher.com>
2020-10-24 08:13:39 -0700

    yes

---
+++ game/src/main/java/jade/Window.java
@@ -0,0 +1,105 @@
+package jade;
+
+import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GL;
+
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+public class Window {
+
+    private int width, height;
+    private String title;
+    private long glfwWindow;
+
+    //fade to black function
+    private float r,g,b,a;
+    private boolean fadeToBlack = false;
+
+    private static Window window = null;
+
+    private Window(){
+        this.width = 1920;
+        this.height = 1080;
+        this.title = "game";
+        r = 1;
+        g = 1;
+        b = 1;
+        a = 1;
+    }
+
+    public static Window get(){
+        if (Window.window == null){
+            Window.window = new Window();
+        }
+        return Window.window;
+    }
+
+    public void run(){
+        System.out.println("hello world " + Version.getVersion());
+
+        init();
+        loop();
+
+        glfwFreeCallbacks(glfwWindow);
+        glfwDestroyWindow(glfwWindow);
+
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
+    }
+
+    public void init(){
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        if (!glfwInit()){
+            throw new IllegalStateException("Unable to initialize GLFW.");
+        }
+
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
+
+        glfwWindow = glfwCreateWindow(this.width, this.height, this.title, NULL, NULL);
+        if (glfwWindow == NULL){
+            throw new IllegalStateException("Failed to create the GLFW window.");
+        }
+
+        glfwSetCursorPosCallback(glfwWindow, MouseListener::mousePosCallback);
+        glfwSetMouseButtonCallback(glfwWindow, MouseListener::mouseButtonCallback);
+        glfwSetScrollCallback(glfwWindow, MouseListener::mouseScrollCallback);
+        glfwSetKeyCallback(glfwWindow, KeyListener::keyCallback);
+
+        glfwMakeContextCurrent(glfwWindow);
+        glfwSwapInterval(1);
+        glfwShowWindow(glfwWindow);
+
+        GL.createCapabilities();
+
+    }
+
+    public void loop(){
+        while(!glfwWindowShouldClose(glfwWindow)){
+            glfwPollEvents();
+
+            glClearColor(r, g, b, a);
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            if (fadeToBlack){
+                r = Math.max(r - 0.0f, 0);
+                g = Math.max(g - 0.01f, 0);
+                b = Math.max(b - 0.0f, 0);
+            }
+
+            if (KeyListener.isKeyPressed(GLFW_KEY_SPACE)){
+                fadeToBlack = true;
+            }
+
+            glfwSwapBuffers(glfwWindow);
+        }
+    }
+
+}
