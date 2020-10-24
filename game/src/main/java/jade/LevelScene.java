7ec1aae111b8dce5fd91cd33eb4fce1c71749810
Suspicious <alex@kaucher.com>
2020-10-24 12:24:32 -0700

    Scene manager entered

---
+++ game/src/main/java/jade/LevelScene.java
@@ -0,0 +1,17 @@
+package jade;
+
+public class LevelScene extends Scene {
+
+    public LevelScene(){
+        System.out.println("Inside level Scene");
+        Window.get().r = 1f;
+        Window.get().g = 1f;
+        Window.get().b = 1f;
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+}
