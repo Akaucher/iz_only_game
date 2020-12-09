6c0d06f9ad96fee5fa87a28d5c7ac82ac651a417
Suspicious <alex@kaucher.com>
2020-12-08 22:26:17 -0700

    Texture loading

--- game/src/main/java/jade/LevelEditorScene.java
+++ game/src/main/java/jade/LevelEditorScene.java
@@ -3,6 +3,8 @@ package jade;
 import org.joml.Vector2f;
 import org.lwjgl.BufferUtils;
 import renderer.Shader;
+import renderer.Texture;
+import util.Time;
 
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
@@ -38,11 +40,11 @@ public class LevelEditorScene extends Scene{
     private int vertexID, fragmentID, shaderProgram;
 
     private float[] vertexArray = {
-        //pos                       //color
-        100.5f, 0.5f, 0.0f,          1.0f, 0.0f, 0.0f, 1.0f,//bottom right   0
-        0.5f, 100.5f, 0.0f,          0.0f, 1.0f, 0.0f, 1.0f,//Top left       1
-        100.5f, 100.5f, 0.0f,           0.0f, 0.0f, 1.0f, 1.0f,//Top right      2
-        0.5f, 0.5f, 0.0f,         1.0f, 1.0f, 0.0f, 1.0f,//Bottom left    3
+        //pos                       //color                     //UV coordinates
+        100f, 0f, 0.0f,          1.0f, 0.0f, 0.0f, 1.0f,        1, 1,//bottom right   0
+        0f, 100f, 0.0f,          0.0f, 1.0f, 0.0f, 1.0f,        0, 0,//Top left       1
+        100f, 100f, 0.0f,        1.0f, 0.0f, 1.0f, 1.0f,        1, 0,//Top right      2
+        0f, 0f, 0.0f,            1.0f, 1.0f, 0.0f, 1.0f,        0, 1//Bottom left     3
 
     };
 
@@ -55,6 +57,7 @@ public class LevelEditorScene extends Scene{
     private int vaoID, vboID, eboID;
 
     private Shader defaultShader;
+    private Texture testTexture;
 
     public LevelEditorScene(){
 
@@ -62,9 +65,11 @@ public class LevelEditorScene extends Scene{
 
     @Override
     public void init(){
-        this.camera = new Camera(new Vector2f());
+        this.camera = new Camera(new Vector2f(-200, -300));
         defaultShader = new Shader("assets/shaders/a.glsl");
         defaultShader.compile();
+        //make sure image has alpha(opacity) of 1
+        this.testTexture = new Texture("assets/images/2474193.jpg"); //images need to be large, like real large//
 
         vaoID = glGenVertexArrays();
         glBindVertexArray(vaoID);
@@ -72,7 +77,7 @@ public class LevelEditorScene extends Scene{
         FloatBuffer vertexBuffer = BufferUtils.createFloatBuffer(vertexArray.length);
         vertexBuffer.put(vertexArray).flip();
 
-        vboID =glGenBuffers();
+        vboID = glGenBuffers();
         glBindBuffer(GL_ARRAY_BUFFER, vboID);
         glBufferData(GL_ARRAY_BUFFER, vertexBuffer, GL_STATIC_DRAW);
 
@@ -85,22 +90,34 @@ public class LevelEditorScene extends Scene{
 
         int positionsSize = 3;
         int colorSize = 4;
-        int floatSizeBytes = 4;
-        int vertexSizeBytes = (positionsSize + colorSize) * floatSizeBytes;
+        int uvSize = 2;
+        int vertexSizeBytes = (positionsSize + colorSize + uvSize) * Float.BYTES;
         glVertexAttribPointer(0, positionsSize, GL_FLOAT, false, vertexSizeBytes, 0);
         glEnableVertexAttribArray(0);
 
-        glVertexAttribPointer(1, colorSize, GL_FLOAT, false, vertexSizeBytes, positionsSize * floatSizeBytes);
+        glVertexAttribPointer(1, colorSize, GL_FLOAT, false, vertexSizeBytes, positionsSize * Float.BYTES);
         glEnableVertexAttribArray(1);
+
+        glVertexAttribPointer(2, uvSize, GL_FLOAT, false, vertexSizeBytes, (positionsSize+colorSize) * Float.BYTES);
+        glEnableVertexAttribArray(2);
     }
 
     @Override
     public void update(float dt) {
-        camera.position.x -= dt * 50.0f;
+        //move noise box
+        //camera.position.x -= dt * 50.0f;
+        //camera.position.y -= dt * 50.0f;
         //bind shader
         defaultShader.use();
+
+        //Upload texture to shader
+        defaultShader.uploadTexture("TEX_SAMPLER", 0);
+        glActiveTexture(GL_TEXTURE0);
+        testTexture.bind();
+
         defaultShader.uploadMat4f("uProjection",camera.getProjectionMatrix());
         defaultShader.uploadMat4f("uView", camera.getViewMatrix());
+        defaultShader.uploadFloat("uTime", Time.getTime());
 
         glBindVertexArray(vaoID);
 
